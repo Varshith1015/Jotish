17e9341e409c218e3e73e067dc2ee128373a70fd
varshith <varshithravipati2003@gmail.com>
2026-03-15 23:52:08 +0530

    Added login function to authContext

--- src/context/authContext.tsx
+++ src/context/authContext.tsx
@@ -2,14 +2,22 @@ import { createContext, useState } from "react";
 
 type AuthContextType = {
   isAuthenticated: boolean;
+  login: (username: string, password: string) => void;
 };
 
 export const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const login = (username: string, password: string) => {
+  if (username === "testuser" && password === "Test123") {
+    setIsAuthenticated(true);
+  } else {
+    alert("Invalid credentials");
+  }
+};
   return (
-    <AuthContext.Provider value={{ isAuthenticated }}>
+    <AuthContext.Provider value={{ isAuthenticated , login }}>
       {children}
     </AuthContext.Provider>
   );
