9f8ad11d44fc2978c6cdc3d7be1d00950263cb1a
varshith <varshithravipati2003@gmail.com>
2026-03-15 23:00:38 +0530

    Created authcontext and auth{rovider skeleton and wrapped our app in authprovider.

---
+++ src/context/authContext.tsx
@@ -0,0 +1,11 @@
+import { createContext } from "react";
+
+export const AuthContext = createContext(null);
+
+export function AuthProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <AuthContext.Provider value={null}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./context/authContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
