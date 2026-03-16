52ca9e5e2745c40c35e04e5f7dbc2ab866466ddb
varshith <varshithravipati2003@gmail.com>
2026-03-16 11:48:01 +0530

    connect login form with AuthContext login function

--- src/context/authContext.tsx
+++ src/context/authContext.tsx
@@ -13,19 +13,21 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
     const [isAuthenticated, setIsAuthenticated] = useState(false);
 
     const login = (username: string, password: string) => {
+        console.log("Login clicked", username, password);
         if (username === "testuser" && password === "Test123") {
+            console.log("Login success");
             setIsAuthenticated(true);
             localStorage.setItem("isAuthenticated", "true");
         } else {
+            console.log("Invalid credentials");
             alert("Invalid credentials");
         }
     };
 
     useEffect(() => {
-    const storedAuth = localStorage.getItem("isAuthenticated");
-
-    if (storedAuth === "true") {
-        setIsAuthenticated(true);
+        const storedAuth = localStorage.getItem("isAuthenticated");
+        if (storedAuth === "true") {
+            setIsAuthenticated(true);
         }
     }, []);
 
--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -1,16 +1,29 @@
 import { useState } from "react";
+import { useContext } from "react";
+import { AuthContext } from "../../context/authContext";
 
 export default function LoginPage(){
 
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const authContext = useContext(AuthContext);
+    const login = authContext?.login;
+    
+    const handleLogin = () => {
+        if (login) {
+            alert("Logging in...");
+            login(username, password);
+        } else {
+            alert("Auth context not available.");
+        }
+    };
 
     return(
         <div>
             <h1>Login</h1>
             <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
             <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-            <button onClick={() => alert("Logging in")}>Login</button>
+            <button onClick={handleLogin}>Login</button>
 
         </div>
     )
