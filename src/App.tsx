7487acca698ac248f0bfd32539ce3e2f416c9228
varshith <varshithravipati2003@gmail.com>
2026-03-16 13:57:13 +0530

    Fetch employee data from API

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import LoginPage from "./pages/Login/LoginPage.tsx";
 import ListPage from "./pages/List/ListPage.tsx";
 import ProtectedRoute from "./components/protectRoute.tsx";
 
+
 export default function App(){
   return (
     <Routes>
 
       <Route path="/login" element={<LoginPage />} />
-      <Route path="/list" element={<ProtectedRoute><h1>List Page</h1></ProtectedRoute>} />   
+      <Route path="/list" element={<ProtectedRoute><ListPage /></ProtectedRoute>} />
       
       
     </Routes>
--- src/pages/List/ListPage.tsx
+++ src/pages/List/ListPage.tsx
@@ -1,4 +1,23 @@
+import { useEffect, useState } from "react";
+
 export default function ListPage(){
+
+    const [employees, setEmployees] = useState([]);
+
+    useEffect(() => {
+        fetch("https://backend.jotish.in/backend_dev/gettabledata.php", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            username: "test",
+            password: "123456"
+        })
+        })
+        console.log(employees);        
+    }, []);
+
     return (
         <div>
             <h1>List Page</h1>
