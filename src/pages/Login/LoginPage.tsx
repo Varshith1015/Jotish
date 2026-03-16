623d5120a83c55221f1b629357d31201338e5083
varshith <varshithravipati2003@gmail.com>
2026-03-16 10:43:28 +0530

    created login form ui with controlled inputs

--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -1,7 +1,17 @@
+import { useState } from "react";
+
 export default function LoginPage(){
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
     return(
         <div>
-            <h1>LoginPage</h1>
+            <h1>Login</h1>
+            <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+            <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            <button onClick={() => alert("Logging in")}>Login</button>
+
         </div>
     )
 }
