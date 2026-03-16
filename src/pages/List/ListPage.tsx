77761efe44b9871bd36516c3c7ab8f2b3016cb6b
varshith <varshithravipati2003@gmail.com>
2026-03-16 19:22:34 +0530

    Track scroll position for virtualization

--- src/pages/List/ListPage.tsx
+++ src/pages/List/ListPage.tsx
@@ -3,6 +3,15 @@ import { useEffect, useState } from "react";
 export default function ListPage(){
 
     const [employees, setEmployees] = useState([]);
+    const [scrollTop, setScrollTop] = useState(0);
+
+    const handleScroll = (event: React.UIEvent<HTMLDivElement>) => {
+        const scrollPosition = event.currentTarget.scrollTop;
+        setScrollTop(scrollPosition);
+        console.log(scrollTop);
+    };
+
+
 
     useEffect(() => {
         fetch("https://backend.jotish.in/backend_dev/gettabledata.php", {
@@ -27,9 +36,37 @@ export default function ListPage(){
     }, []);
 
     return (
-        <div>
+        <div style={{ height: "500px", overflowY: "auto" }} onScroll={handleScroll}>
             <h1>List Page</h1>
             <p>Total Employees: {employees.length}</p>
+            <table>
+                <thead>
+                    <tr>
+                    <th>Name</th>
+                    <th>Position</th>
+                    <th>City</th>
+                    <th>Extension</th>
+                    <th>StartDate</th>
+                    <th>Salary</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {employees.map((employee: any, index: number) => (
+                    <tr key={index}>
+                        <td>{employee[0]}</td>
+                        <td>{employee[1]}</td>
+                        <td>{employee[2]}</td>
+                        <td>{employee[3]}</td>
+                        <td>{employee[4]}</td>
+                        <td>{employee[5]}</td>
+                    </tr>
+                    ))}
+                </tbody>
+            </table>
+
+            
+
         </div>
     )
 }
