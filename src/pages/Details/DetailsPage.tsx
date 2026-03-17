e533849081f2090662891b5ff592bf018e46bff6
varshith <varshithravipati2003@gmail.com>
2026-03-17 16:43:40 +0530

    Access camera and display video stream

--- src/pages/Details/DetailsPage.tsx
+++ src/pages/Details/DetailsPage.tsx
@@ -1,7 +1,37 @@
+import { useEffect, useRef } from "react";
+
+
 export default function DetailsPage(){
+    
+    const videoRef = useRef<HTMLVideoElement | null>(null);
+
+    useEffect(() => {
+    const startCamera = async () => {
+        try {
+        const stream = await navigator.mediaDevices.getUserMedia({
+            video: true
+        });
+
+        if (videoRef.current) {
+            videoRef.current.srcObject = stream;
+        }
+        } catch (error) {
+        console.error("Error accessing camera:", error);
+        }
+    };
+
+    startCamera();
+    }, []);
+
     return(
         <div>
-            <h1>Employee Details Page</h1>
+            <h1>Employee Details</h1>
+            <video
+                ref={videoRef}
+                autoPlay
+                playsInline
+                style={{ width: "400px", border: "1px solid black" }}
+            />
         </div>
     )
 };
